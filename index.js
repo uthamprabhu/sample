5c7badc441603573e8ff75b0d2607664bece2e83
uthamprabhu <uthamprabhu1@gmail.com>
2023-01-12 14:56:52 +0530

    index page edited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hellloo this is response");
+  res.send("hellloo World");
 });
 
 app.listen(4000, () => console.log("server runnig in port 4000"));
